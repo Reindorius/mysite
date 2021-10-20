e159e7c4432ebea5f60c3075099ee4efdd21fa8c
Hongxuan Yue <conquistadore@126.com>
2021-10-20 17:57:08 +0800

    initial commit

---
+++ news/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db.models.base import ModelState
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+    
